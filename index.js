b87cd8fff06526c8d370cfa40eedbb111b4350e2
honorable-scraper-2425 <kdraper93@gmail.com>
2019-06-28 18:36:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function updateObjectWithKeyAndValue (obj, key, value) {
   return Object.assign({}, obj, {[key]:value})
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+function destructivelyUpdateObjectWithKeyAndValue (obj, key, value) {
   recipes[key] = value
   return recipes
 }
